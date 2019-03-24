712f19e06c18cdb64c8df7496c0fc4a1f076f1d1
Steven <enomeral@gmx.de>
2019-03-24 11:23:56 +0100

    implement Height (of area)

---
+++ src/main/java/unit/adjustment/supplier/Height.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 Upyter
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package unit.adjustment.supplier;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+import unit.area.Area;
+
+/**
+ * The height of an area.
+ * <p>This class is immutable and thread-safe.</p>
+ * @since 0.78
+ */
+public class Height implements Supplier<Integer> {
+    /**
+     * The area to use the height from.
+     */
+    private final Area area;
+
+    /**
+     * Ctor.
+     * @param area The area to use the height from.
+     */
+    public Height(final Area area) {
+        this.area = Objects.requireNonNull(area);
+    }
+
+    @Override
+    public final Integer get() {
+        return Area.result(
+            this.area,
+            (x, y, width, height) -> height
+        );
+    }
+}
