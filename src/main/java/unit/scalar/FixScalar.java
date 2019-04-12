45528b0beb7a11b6ba2e1e30ee1dd82f7c8a5e65
Steven <enomeral@gmx.de>
2019-04-12 12:13:35 +0200

    implement FixScalar

---
+++ src/main/java/unit/scalar/FixScalar.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 Upyter
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package unit.scalar;
+
+import java.util.Objects;
+import unit.functional.FloatSupplier;
+
+/**
+ * An one-dimensional value.
+ * <p>Whether this class is immutable or thread-safe depends on the constructor
+ * arguments.</p>
+ * @see unit.pos.Pos
+ * @see unit.size.Size
+ * @since 0.97
+ */
+public class FixScalar implements FloatSupplier {
+    /**
+     * The supplier that gives this scalar its value.
+     */
+    private final FloatSupplier supplier;
+
+    /**
+     * Uses 0.0F as its value.
+     */
+    public FixScalar() {
+        this(0.0F);
+    }
+
+    /**
+     * Ctor.
+     * @param value The value that this scalar will have.
+     */
+    public FixScalar(final float value) {
+        this(() -> value);
+    }
+
+    /**
+     * Ctor.
+     * @param supplier The supplier that gives this scalar its value.
+     */
+    public FixScalar(final FloatSupplier supplier) {
+        this.supplier = Objects.requireNonNull(supplier);
+    }
+
+    @Override
+    public final float getAsFloat() {
+        return this.supplier.getAsFloat();
+    }
+}
---
+++ src/test/java/unit/scalar/FixScalarTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2018 Upyter
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package unit.scalar;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import unit.functional.FloatSupplier;
+import unit.number.Delta;
+
+/**
+ * Tests for {@link FixScalar}.
+ * @since 0.97
+ */
+public final class FixScalarTest {
+    /**
+     * {@link FixScalar#getAsFloat()} must return the value of the supplier when
+     * constructed with {@link FixScalar#FixScalar(FloatSupplier)}.
+     */
+    @Test
+    public void fixedSupplierValue() {
+        final float value = 455.453F;
+        MatcherAssert.assertThat(
+            (double) new FixScalar(
+                () -> value
+            ).getAsFloat(),
+            Matchers.closeTo(
+                (double) value,
+                new Delta((double) value).value())
+        );
+    }
+
+    /**
+     * {@link FixScalar#getAsFloat()} must return the value of the supplier even
+     * if its changing when constructed with
+     * {@link FixScalar#FixScalar(FloatSupplier)}.
+     */
+    @Test
+    public void reactsToChangingSupplier() {
+        final float first = 438.23F;
+        final float second = 6.3F;
+        final FloatSupplier supplier = new FloatSupplier() {
+            private boolean called = false;
+
+            @Override
+            public float getAsFloat() {
+                final float result;
+                if (this.called) {
+                    result = second;
+                } else {
+                    result = first;
+                    this.called = true;
+                }
+                return result;
+            }
+        };
+        MatcherAssert.assertThat(
+            (double) new FixScalar(supplier).getAsFloat(),
+            Matchers.closeTo(
+                (double) first,
+                new Delta((double) first).value())
+        );
+        MatcherAssert.assertThat(
+            (double) new FixScalar(supplier).getAsFloat(),
+            Matchers.closeTo(
+                (double) second,
+                new Delta((double) second).value())
+        );
+    }
+
+    /**
+     * {@link FixScalar#getAsFloat()} must return the given value when
+     * constructed with {@link FixScalar#FixScalar(float)}.
+     */
+    @Test
+    public void floatConstructor() {
+        final float value = 632.45F;
+        MatcherAssert.assertThat(
+            (double) new FixScalar(value).getAsFloat(),
+            Matchers.closeTo(
+                (double) value,
+                new Delta((double) value).value())
+        );
+    }
+
+    /**
+     * {@link FixScalar#getAsFloat()} must return 0.0 when constructed with
+     * {@link FixScalar#FixScalar()}.
+     */
+    @Test
+    public void defaultConstructor() {
+        MatcherAssert.assertThat(
+            (double) new FixScalar().getAsFloat(),
+            Matchers.closeTo(0.0, new Delta(0.0).value())
+        );
+    }
+}
